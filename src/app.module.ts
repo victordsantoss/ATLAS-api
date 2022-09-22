7bebaf6948d1a7e8533e57a4aae731f173876868
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2022-09-22 12:35:00 -0300

    chore: updating configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
+import { BrUfModule } from './modules/br_uf.module';
 import { StatesModule } from './modules/states.module';
 
 @Module({
   imports: [
     DatabaseModule,
-    StatesModule
+    StatesModule,
+    BrUfModule
   ],
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix(`atlas/`);
   const config = new DocumentBuilder()
     .setTitle('ATLAS NestJs API')
     .setDescription('The API description')
